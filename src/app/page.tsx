bc37618adb6d57b7154bdc297a75509aabf49e15
thienphuoc <thienphuoc2391@gmail.com>
2023-05-30 22:17:28 +0700

    create login with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Button from '@/components/ui/Button';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>hello</Button>
-    </main>
-  );
+  return <Button variant="ghost">Hello</Button>;
 }
