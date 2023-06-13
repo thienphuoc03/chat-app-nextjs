d3b8d31f281039b619cb39bc32558fe4635dd95a
thienphuoc <thienphuoc2391@gmail.com>
2023-06-13 19:01:23 +0700

    update config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,18 +1,19 @@
-import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter';
 import { NextAuthOptions } from 'next-auth';
+import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter';
 import { db } from './db';
 import GoogleProvider from 'next-auth/providers/google';
+import { fetchRedis } from '@/helpers/redis';
 
 function getGoogleCredentials() {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
   if (!clientId || clientId.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_ID!!!');
+    throw new Error('Missing GOOGLE_CLIENT_ID');
   }
 
   if (!clientSecret || clientSecret.length === 0) {
-    throw new Error('Missing GOOGLE_CLIENT_SECRET!!!');
+    throw new Error('Missing GOOGLE_CLIENT_SECRET');
   }
 
   return { clientId, clientSecret };
@@ -23,6 +24,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
+
   pages: {
     signIn: '/login',
   },
@@ -34,13 +36,19 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user }) {
-      const dbUser = (await db.get(`user: ${token.id}`)) as User | null;
+      const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as
+        | string
+        | null;
 
-      if (!dbUser) {
-        token.id = user!.id;
+      if (!dbUserResult) {
+        if (user) {
+          token.id = user!.id;
+        }
 
         return token;
       }
+
+      const dbUser = JSON.parse(dbUserResult) as User;
 
       return {
         id: dbUser.id,
@@ -49,7 +57,6 @@ export const authOptions: NextAuthOptions = {
         picture: dbUser.image,
       };
     },
-
     async session({ session, token }) {
       if (token) {
         session.user.id = token.id;
@@ -60,7 +67,6 @@ export const authOptions: NextAuthOptions = {
 
       return session;
     },
-
     redirect() {
       return '/dashboard';
     },
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Merges multiple class names into a single string.
+ *
+ * @param inputs - An array of class names or class name objects.
+ * @returns A string containing all merged class names.
+ */
 export function cn(...inputs: ClassValue[]) {
+  // Merge all class names into a single string.
   return twMerge(clsx(inputs));
+}
+
+export function toPusherKey(key: string) {
+  return key.replace(/:/g, '__');
+}
+
+export function chatHrefConstructor(id1: string, id2: string) {
+  const sortedIds = [id1, id2].sort();
+  return `${sortedIds[0]}--${sortedIds[1]}`;
 }
